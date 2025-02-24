ee869be6db84ebf04fc60f00b3986c7054b723d3
CHillcode931 <Charleswhill931@gmail.com>
2025-02-24 10:15:00 -0600

    got pics

--- scripts.js
+++ scripts.js
@@ -11,24 +11,38 @@ const getPokemon = async()=>{
         console.error(error);
     }
 };
-
+const getPokeDetails=async(url)=>{
+    try {
+        let response = await fetch(
+            url
+        );
+        let data = await response.json()
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+    
 const p = getPokemon()
 .then((data) => {
     console.log(data)
-    data.results.map((pokemon)=>{
+    data.results.map((pokemon,index)=>{
+    
+    
         const para= document.createElement("p");
         const div= document.createElement("div");
         para.append(pokemon.name)
         const photo=document.createElement("img");
-    photo.src=
+        photo.src=`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1 }.png`;
         div.append(para,photo);
-    let root=document.getElementById('root');
-
-    //creating a class name for div
-    //and appending div to root
-    div.classList.add("card")
-    root.append(div);
+        let root=document.getElementById('root');
+        
+        
+        //creating a class name for div
+        //and appending div to root
+        div.classList.add("card")
+        root.append(div);
     });
-
+    
 })
 .catch((error)=>console.error(error));
